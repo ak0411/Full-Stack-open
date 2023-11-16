1def9386470362852bccde0373d1662bad4a5a0f
Anawil Karavek <akaravek@hotmail.com>
2023-11-16 18:09:48 +0100

    done 1.14

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+
+const Header = ({text}) => <h1>{text}</h1>
+
+const Anecdote = ({text, votes}) => <div>{text}<br/>has {votes} votes</div>
+
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(Array(anecdotes.length).fill(0))
+  const [mostVote, setMostVote] = useState(0)
+
+  const handleAnecdote = () => {
+    const index = Math.floor(Math.random() * anecdotes.length)
+    setSelected(index)
+  }
+
+  const handleVote = () => {
+    const copy = [...points]
+    copy[selected] += 1
+    setPoints(copy)
+
+    if (copy[selected] > copy[mostVote]) {
+      setMostVote(selected)
+    }
+  }
+
+  return (
+    <>
+      <Header text="Anecdote of the day" />
+      <Anecdote text={anecdotes[selected]} votes={points[selected]} />
+      <Button onClick={handleVote} text="vote" />
+      <Button onClick={handleAnecdote} text="next anecdote" />
+      <Header text="Anecdote with most vote" />
+      <Anecdote text={anecdotes[mostVote]} votes={points[mostVote]} />
+    </>
+  )
+}
+
+export default App
