29477bf01790167564cea6b166ac31c87b5ca5d4
Anawil Karavek <akaravek@hotmail.com>
2023-11-20 22:04:49 +0100

    done 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,15 @@
 const express = require('express')
+var morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+
+morgan.token('body', function getBody (req) {
+    console.log(req)
+    return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     { 
