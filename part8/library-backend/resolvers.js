33a21f8a2b8e61d3faa5927ac5f518dc64a51ddf
Anawil Karavek <akaravek@hotmail.com>
2023-12-16 13:52:56 +0100

    refactoring backend

---
+++ part8/library-backend/resolvers.js
@@ -0,0 +1,146 @@
+const Book = require('./models/book')
+const Author = require('./models/author')
+const User = require('./models/user')
+const { GraphQLError } = require('graphql')
+const jwt = require('jsonwebtoken')
+
+const resolvers = {
+  Query: {
+    bookCount: async () => Book.collection.countDocuments(),
+    authorCount: async () => Author.collection.countDocuments(),
+    allBooks: async (root, args) => {
+      if (args.author) {
+        const author = await Author.findOne({ name: args.author })
+
+        if (!author) {
+          return null
+        }
+
+        if (args.genre) {
+          return await Book.find({ author: author._id, genres: args.genre }).populate('author')
+        } else {
+          return await Book.find({ author: author._id }).populate('author')
+        }
+      } else if (args.genre) {
+        return await Book.find({ genres: args.genre }).populate('author')
+      }
+
+      return await Book.find({}).populate('author')
+    },
+    allAuthors: async () => Author.find({}),
+    me: (root, args, context) => context.currentUser,
+  },
+  Author: {
+    bookCount: async (root) => Book.find({ author: root.id }).countDocuments()
+  },
+  Mutation: {
+    addBook: async (root, args, { currentUser }) => {
+      if (!currentUser) {
+        throw new GraphQLError('not authenticated', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+          }
+        })
+      }
+
+      const foundAuthor = await Author.findOne({ name: args.author })
+
+      let bookAuthor
+      if (!foundAuthor) {
+        const author = new Author({ name: args.author })
+
+        try {
+          bookAuthor = await author.save()
+        } catch (error) {
+          throw new GraphQLError('Saving author failed', {
+            extensions: {
+              code: 'BAD_USER_INPUT',
+              invalidArgs: args.author,
+              error
+            }
+          })
+        }
+      }
+
+      const author = bookAuthor
+        ? bookAuthor
+        : foundAuthor
+
+      const book = new Book({ ...args, author })
+
+      return book.save()
+        .catch(error => {
+          throw new GraphQLError('Saving book failed', {
+            extensions: {
+              code: 'BAD_USER_INPUT',
+              error
+            }
+          })
+        })
+    },
+    editAuthor: async (root, args, { currentUser }) => {
+      if (!currentUser) {
+        throw new GraphQLError('not authenticated', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+          }
+        })
+      }
+
+      const author = await Author.findOne({ name: args.name })
+      if (!author) {
+        return null
+      }
+
+      author.born = args.setBornTo
+
+      try {
+        await author.save()
+      } catch (error) {
+        throw new GraphQLError('Saving born year failed', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.setBornTo,
+            error
+          }
+        })
+      }
+
+      return author
+    },
+    createUser: async (root, args) => {
+      const user = new User({ username: args.username, favoriteGenre: args.favoriteGenre })
+
+      return user.save()
+        .catch(error => {
+          throw new GraphQLError('Creating the user failed', {
+            extensions: {
+              code: 'BAD_USER_INPUT',
+              invalidArgs: args.username,
+              error
+            }
+          })
+        })
+    },
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username })
+
+      if ( !user || args.password !== 'secret' ) {
+        throw new GraphQLError('wrong credentials', {
+          extensions: {
+            code: 'BAD_USER_INPUT'
+          }
+        })
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      }
+
+      return { value: jwt.sign(userForToken, process.env.JWT_SECRET) }
+    },
+  }
+}
+
+module.exports = resolvers
